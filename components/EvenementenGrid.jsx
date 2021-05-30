eb75b73d165e9d15adf14a977557172f40c2a845
roel-vereycken <vereyckenroel@gmail.com>
2021-05-31 00:26:53 +0200

    pagenr

--- components/EvenementenGrid.jsx
+++ components/EvenementenGrid.jsx
@@ -1,25 +1,18 @@
-import React from 'react'
 import {
     Box,
     IconButton,
     Button,
     Center,
     Divider,
-    Flex,
-    Heading,
-    Input,
-    Menu,
-    MenuButton,
-    MenuList,
-    MenuItem,
     SimpleGrid,
     Image,
     Text,
   } from "@chakra-ui/react";
-  import { ChevronLeftIcon, ChevronRightIcon } from "@chakra-ui/icons";
+import Link from 'next/link'
+
 
 function EvenementenGrid({data}) {
-  console.log(data)
+    console.log(data)
     return (
         <>
         <Box width="100%">
@@ -31,8 +24,9 @@ function EvenementenGrid({data}) {
             spacingX="50px"
             spacingY="50px"
           >
-            {data["hydra:member"].map((evenement) => {
+            {data.map((evenement) => {
               return (
+                <Link href="/evenement/1">
                 <a id="eventLink">
                 <Box
                   bg="gray.50"
@@ -71,29 +65,11 @@ function EvenementenGrid({data}) {
                   </Text>
                 </Box>
                 </a>
+                </Link>
               );
             })}
           </SimpleGrid>
-          <Center>
-            <Box marginTop="35px">
-              <IconButton
-                height="30px"
-                width="30px"
-                icon={<ChevronLeftIcon />}
-                border="1px solid grey"
-              />
-              <Button height="30px" width="30px" marginLeft="10px" border="1px solid grey" >
-                1
-              </Button>
-              <IconButton
-                marginLeft="10px"
-                height="30px"
-                width="30px"
-                icon={<ChevronRightIcon />}
-                border="1px solid grey"
-              />
-            </Box>
-          </Center>
+          
         </Box>
         </>
     )
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,39 @@
 process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
+import React, { useState, useEffect } from "react";
 
 import axios from "axios";
-import { Box, Flex } from "@chakra-ui/react";
+import { Box, Flex, IconButton, Button, Center } from "@chakra-ui/react";
+import { ChevronLeftIcon, ChevronRightIcon } from "@chakra-ui/icons";
+
 import Navbar from "../components/Navbar";
 import EvenementenGrid from "../components/EvenementenGrid";
 import Searchbar from "../components/Searchbar";
 
 export default function Home({ data }) {
-  console.log(data["hydra:view"]);
+  // console.log(data["hydra:view"]);
+
+  // const [currentPage, setCurrentPage] = useState(1);
+  // const [pageNumber, setPageNumber] = useState(1);
+  // const [pageNumberArray, setPageNumberArray] = useState([]);
+  // const [data, setData] = useState([]);
+
+  // useEffect(async () => {
+  //   const resp = await axios.get(
+  //     `https://127.0.0.1:8000/api/events.jsonld?page=${currentPage}`
+  //   );
+  //   setData(resp.data);
+
+  // }, []);
+
+  const handlepageNumberClick = (e) => {
+    console.log(e.target.id);
+    setCurrentPage(e.target.id);
+  };
+
+  console.log("data?", data["hydra:member"]);
+  const pageNumber = Math.ceil(data["hydra:totalItems"] / 6);
+  const pageNumberArray = [...Array(pageNumber).keys()].map((page) => page + 1);
+
   return (
     <>
       <Flex>
@@ -16,7 +42,39 @@ export default function Home({ data }) {
         </Box>
         <Box bgColor="gray.200" width="80%" minHeight="100vh">
           <Searchbar data={data} />
-          <EvenementenGrid data={data} />
+          <EvenementenGrid data={data["hydra:member"]} />
+
+          <Center>
+            <Box marginTop="35px">
+              <IconButton
+                height="30px"
+                width="30px"
+                icon={<ChevronLeftIcon />}
+                border="1px solid grey"
+              />
+              {pageNumberArray.map((pagenr) => (
+                <Button
+                  key={pagenr}
+                  id={pagenr}
+                  height="30px"
+                  width="30px"
+                  marginLeft="10px"
+                  border="1px solid grey"
+                  onClick={handlepageNumberClick}
+                >
+                  {pagenr}
+                </Button>
+              ))}
+
+              <IconButton
+                marginLeft="10px"
+                height="30px"
+                width="30px"
+                icon={<ChevronRightIcon />}
+                border="1px solid grey"
+              />
+            </Box>
+          </Center>
         </Box>
       </Flex>
     </>
